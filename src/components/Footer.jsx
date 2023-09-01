566182dab196b6d94edff6850100fcf211d629fc
truthixify <truthixify@gmailcom>
2023-09-02 00:41:25 +0100

    open links in new tab

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <footer className='flex-lg footer'>
       <p className='flex-md'>&copy; 2023 Simple Note Editor. All rights reserved.</p>
       <div className='flex-md'>
-        <SocialIcon url='https://github.com/Truthixify' />
-        <SocialIcon url='https://twitter.com/truthixifi' />
-        <SocialIcon url='https://www.linkedin.com/in/wasiu-ajao-40956428b/' />
-        <SocialIcon url='https://api.whatsapp.com/send?phone=2348105309925' />
+        <SocialIcon url='https://github.com/Truthixify' target='_blank' />
+        <SocialIcon url='https://twitter.com/truthixifi' target='_blank' />
+        <SocialIcon url='https://www.linkedin.com/in/wasiu-ajao-40956428b/' target='_blank' />
+        <SocialIcon url='https://api.whatsapp.com/send?phone=2348105309925' target='_blank' />
       </div>
     </footer>
   )
